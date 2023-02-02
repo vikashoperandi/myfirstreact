feb6d3693640fe743c3163df711a15aa9f1c8330
vikash mishra <vikash.mishra@operandi.codes>
2023-02-02 12:41:32 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React,{useState}  from 'react'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome react
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='calc'>
+     <input type="text" placeholder='0' id='ans'/>
+    
+     <input type='button'value='AC' className='button'/>
+     <input type='button'value='+/-'className='button' />
+     <input type='button'value='%'className='button'/>
+     <input type='button'value='/'className='button'/>
+     <input type='button'value='7'className='button'/>
+     <input type='button'value='8'className='button'/>
+     <input type='button'value='9'className='button'/>
+     <input type='button'value='x'className='button'/>
+     <input type='button'value='4'className='button'/>
+     <input type='button'value='5'className='button'/>
+     <input type='button'value='6'className='button'/>
+     <input type='button'value='-'className='button'/>
+     <input type='button'value='1'className='button'/>
+     <input type='button'value='2'className='button'/>
+     <input type='button'value='3'className='button'/>
+     <input type='button'value='+'className='button'/>
+     <input id='zero' type='button'value='0'className='button'/>
+     <input type='button'value='.'className='button'/>
+     <input type='button'value='='className='button'/>
+     
+    
+     
     </div>
-  );
+    
+  )
 }
 
 export default App;
