db64508cc2bf49141273ffb2eb3b8904ad1fd100
vikash mishra <vikash.mishra@operandi.codes>
2023-02-02 16:04:59 +0530

    all func done

--- src/App.js
+++ src/App.js
@@ -8,30 +8,46 @@ const clickhandler=(event)=>{
    setresult(result.concat(event.target.value))
 
 }
+const clear=()=>{
+  setresult("");
+}
+const total=()=>{
+   let res = Function("return "+result)();
+  setresult(res.toString());
+
+}
+ const plusmines=()=>{
+  if(result.charAt(0)==="-"){
+  setresult(result.substring(1));
+  }else{
+    setresult("-"+result);
+  }
+   
+ }
 
   return (
     <div className='calc'>
      <input type="text" placeholder='0' id='ans' value={result}/>
     
-     <input type='button'value='AC' className='button'/>
-     <input type='button'value='+/-'className='button' onClick={clickhandler}/>
+     <input type='button'value='AC' className='button'onClick={clear}/>
+     <input type='button'value='+/-'className='button' onClick={plusmines}/>
      <input type='button'value='%'className='button'onClick={clickhandler}/>
-     <input type='button'value='/'className='button'onClick={clickhandler}/>
+     <input type='button'value='/' id='d1'className='button'onClick={clickhandler}/>
      <input type='button'value='7'className='button' onClick={clickhandler}/>
      <input type='button'value='8'className='button'onClick={clickhandler}/>
      <input type='button'value='9'className='button'onClick={clickhandler}/>
-     <input type='button'value='x'className='button'onClick={clickhandler}/>
+     <input type='button'value='*'id='d1'className='button'onClick={clickhandler}/>
      <input type='button'value='4'className='button'onClick={clickhandler}/>
      <input type='button'value='5'className='button'onClick={clickhandler}/>
      <input type='button'value='6'className='button'onClick={clickhandler}/>
-     <input type='button'value='-'className='button'onClick={clickhandler}/>
+     <input type='button'value='-'id='d1'className='button'onClick={clickhandler}/>
      <input type='button'value='1'className='button'onClick={clickhandler}/>
      <input type='button'value='2'className='button'onClick={clickhandler}/>
      <input type='button'value='3'className='button'onClick={clickhandler}/>
-     <input type='button'value='+'className='button'onClick={clickhandler}/>
+     <input type='button'value='+'id='d1'className='button'onClick={clickhandler}/>
      <input id='zero' type='button'value='0'className='button'onClick={clickhandler}/>
      <input type='button'value='.'className='button'onClick={clickhandler}/>
-     <input type='button'value='='className='button'/>
+     <input type='button'value='='id='d1'className='button' onClick={total}/>
      
     
      
